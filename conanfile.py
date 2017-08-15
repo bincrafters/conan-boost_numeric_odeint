ba4c82131a04fa4cf86c10fb764b8c5856ea5d96
Jerry W <jerrywiltse@gmail.com>
2017-08-15 18:03:47 -0400

    catching up this repo

--- conanfile.py
+++ conanfile.py
@@ -3,11 +3,11 @@ from conans import ConanFile, tools, os
 class BoostNumeric_OdeintConan(ConanFile):
     name = "Boost.Numeric_Odeint"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/odeint"
+    url = "https://github.com/bincrafters/conan-boost-odeint"
+    source_url = "https://github.com/boostorg/odeint"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    lib_short_name = "odeint"
+    lib_short_names = ["odeint"]
     requires =  "Boost.Array/1.64.0@bincrafters/testing", \
                       "Boost.Assert/1.64.0@bincrafters/testing", \
                       "Boost.Bind/1.64.0@bincrafters/testing", \
@@ -33,12 +33,14 @@ class BoostNumeric_OdeintConan(ConanFile):
                       #array3 assert1 bind3 compute14 config0 core2 function5 fusion5 iterator5 math8 mpi14 mpl5 multi_array6 numeric~ublas12 preprocessor0 range7 static_assert1 throw_exception2 type_traits3 units12 utility5
                       
     def source(self):
-        self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+        for lib_short_name in self.lib_short_names:
+            self.run("git clone --depth=50 --branch=boost-{0} https://github.com/boostorg/{1}.git"
+                     .format(self.version, lib_short_name)) 
 
     def package(self):
-        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
-        self.copy(pattern="*", dst="", src=include_dir)
+        for lib_short_name in self.lib_short_names:
+            include_dir = os.path.join(lib_short_name, "include")
+            self.copy(pattern="*", dst="include", src=include_dir)		
 
     def package_id(self):
         self.info.header_only()
