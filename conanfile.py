d4d74049c4714a8bd8a78a52947bed9f03fe4ff1
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-30 12:25:05 -0400

    Fixed source methods and a few package methods

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,6 @@ class BoostNumeric_OdeintConan(ConanFile):
     version = "1.64.0"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-odeint"
-    source_url = "https://github.com/boostorg/odeint"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["odeint"]
@@ -34,9 +33,12 @@ class BoostNumeric_OdeintConan(ConanFile):
                       #array3 assert1 bind3 compute14 config0 core2 function5 fusion5 iterator5 math8 mpi14 mpl5 multi_array6 numeric~ublas12 preprocessor0 range7 static_assert1 throw_exception2 type_traits3 units12 utility5
                       
     def source(self):
+        boostorg_github = "https://github.com/boostorg"
+        archive_name = "boost-" + self.version  
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
-                     .format(self.version, lib_short_name)) 
+            tools.get("{0}/{1}/archive/{2}.tar.gz"
+                .format(boostorg_github, lib_short_name, archive_name))
+            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
 
     def package(self):
         for lib_short_name in self.lib_short_names:
