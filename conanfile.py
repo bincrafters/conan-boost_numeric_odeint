57f11b9d3a6fd13b6f896ba6a9a09f97e1a1d6ad
Rene Rivera <grafikrobot@gmail.com>
2018-01-29 09:53:49 -0600

    Merge.

--- conanfile.py
+++ conanfile.py
@@ -17,28 +17,28 @@ class BoostNumeric_OdeintConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/stable",
-        "boost_array/1.66.0@bincrafters/stable",
-        "boost_assert/1.66.0@bincrafters/stable",
-        "boost_bind/1.66.0@bincrafters/stable",
-        "boost_compute/1.66.0@bincrafters/stable",
-        "boost_config/1.66.0@bincrafters/stable",
-        "boost_core/1.66.0@bincrafters/stable",
-        "boost_function/1.66.0@bincrafters/stable",
-        "boost_fusion/1.66.0@bincrafters/stable",
-        "boost_iterator/1.66.0@bincrafters/stable",
-        "boost_math/1.66.0@bincrafters/stable",
-        "boost_mpi/1.66.0@bincrafters/stable",
-        "boost_mpl/1.66.0@bincrafters/stable",
-        "boost_multi_array/1.66.0@bincrafters/stable",
-        "boost_numeric_ublas/1.66.0@bincrafters/stable",
-        "boost_preprocessor/1.66.0@bincrafters/stable",
-        "boost_range/1.66.0@bincrafters/stable",
-        "boost_static_assert/1.66.0@bincrafters/stable",
-        "boost_throw_exception/1.66.0@bincrafters/stable",
-        "boost_type_traits/1.66.0@bincrafters/stable",
-        "boost_units/1.66.0@bincrafters/stable",
-        "boost_utility/1.66.0@bincrafters/stable"
+        "boost_package_tools/1.66.0@bincrafters/testing",
+        "boost_array/1.66.0@bincrafters/testing",
+        "boost_assert/1.66.0@bincrafters/testing",
+        "boost_bind/1.66.0@bincrafters/testing",
+        "boost_compute/1.66.0@bincrafters/testing",
+        "boost_config/1.66.0@bincrafters/testing",
+        "boost_core/1.66.0@bincrafters/testing",
+        "boost_function/1.66.0@bincrafters/testing",
+        "boost_fusion/1.66.0@bincrafters/testing",
+        "boost_iterator/1.66.0@bincrafters/testing",
+        "boost_math/1.66.0@bincrafters/testing",
+        "boost_mpi/1.66.0@bincrafters/testing",
+        "boost_mpl/1.66.0@bincrafters/testing",
+        "boost_multi_array/1.66.0@bincrafters/testing",
+        "boost_numeric_ublas/1.66.0@bincrafters/testing",
+        "boost_preprocessor/1.66.0@bincrafters/testing",
+        "boost_range/1.66.0@bincrafters/testing",
+        "boost_static_assert/1.66.0@bincrafters/testing",
+        "boost_throw_exception/1.66.0@bincrafters/testing",
+        "boost_type_traits/1.66.0@bincrafters/testing",
+        "boost_units/1.66.0@bincrafters/testing",
+        "boost_utility/1.66.0@bincrafters/testing"
     )
 
     # BEGIN
@@ -46,7 +46,7 @@ class BoostNumeric_OdeintConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/stable"
+    build_requires = "boost_generator/1.66.0@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
