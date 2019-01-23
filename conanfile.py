42aed83c1906b2cdf126aaa7a20c56f1feaeb8cb
Jerry W <jerrywiltse@gmail.com>
2019-01-22 21:57:29 -0500

    use clang 5.0 for header only, add clang 7 to others, implement versions in recipes, conan no longer allows inheriting version, refresh travis badge, use .ci dir, python 3.6 in appveyor

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.67.0@bincrafters/testing")
 
 class BoostNumeric_OdeintConan(base.BoostBaseConan):
     name = "boost_numeric_odeint"
+    version = "1.67.0"
     url = "https://github.com/bincrafters/conan-boost_numeric_odeint"
     lib_short_names = ["odeint"]
     header_only_libs = ["odeint"]
